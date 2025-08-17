e3fb33b1bcb5b862ce0b44cb0120aa55d56582ea
Simplify <jonghun.criss.android@gmail.com>
2025-08-17 22:02:44 +0900

    add hello

---
+++ 04/hello/src/main/kotlin/com/springboot/hello/controller/HelloController.kt
@@ -0,0 +1,13 @@
+package com.springboot.hello.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @RequestMapping("/hello")
+    fun hello() : String {
+        return "Hello World!"
+    }
+}
